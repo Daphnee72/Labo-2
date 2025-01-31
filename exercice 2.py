abfb8b7f682126387f766e738d945bbd7807ff81
Daphnee72 <daphneepelletier18@icloud.com>
2025-01-31 14:50:17 -0500

    Create exercice 2.py

---
+++ exercice 2.py
@@ -0,0 +1,8 @@
+a = input
+nombre_str = input() #entrer un nombre
+nombre_int = int(nombre_str) #convertir le nombre en entier
+nombre_float = float(nombre_str) #convertir en nombre decimal
+
+# resultat
+print(nombre_int)
+print(nombre_float)
