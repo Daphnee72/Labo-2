4113dfbb0a79629745c415f3fde9c534e0bca1a9
Daphnee72 <daphneepelletier18@icloud.com>
2025-01-31 15:59:17 -0500

    Update exercice 6.py

--- exercice 6.py
+++ exercice 6.py
@@ -1,4 +1,3 @@
-nombre = input("entrer un nombre: ")
+nombre = int (input("entrer un nombre:")) #car c'est un nombre pour les deecimaux
 resultat = nombre * 2
-print ("le double de votre nombre est: ")
-resultat = ("le logitiel met la valeur")
+print ("le double de votre nombre est:",resultat)
